2435085a014355b01fbb0beb4b628d42fb262149
Vishal Gaikwad <vishalgofficial@gmail.com>
2020-04-19 15:59:38 +0530

    initial

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+
+export default class Sidebar extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <nav
+            href="#navbar"
+            className="js-colorlib-nav-toggle colorlib-nav-toggle"
+            data-toggle="collapse"
+            data-target="#navbar"
+            aria-expanded="false"
+            aria-controls="navbar"
+          >
+            <i />
+          </nav>
+          <aside id="colorlib-aside" className="border js-fullheight">
+            <div className="text-center">
+              <div
+                className="author-img"
+                style={{ backgroundImage: "url(images/about.jpg)" }}
+              />
+              <h1 id="colorlib-logo">
+                <a href="index.html">Vishal Gaikwad</a>
+              </h1>
+              <span className="email">
+                <i className="icon-mail"></i> vishalgofficial@gmail.com
+              </span>
+            </div>
+            <nav id="colorlib-main-menu" role="navigation" className="navbar">
+              <div id="navbar" className="collapse">
+                <ul>
+                  <li className="active">
+                    <a href="#home" data-nav-section="home">
+                      Introduction
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#about" data-nav-section="about">
+                      About
+                    </a>
+                  </li>
+                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
+                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
+                  <li>
+                    <a href="#timeline" data-nav-section="timeline">
+                      Timeline
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <nav id="colorlib-main-menu">
+              <ul>
+                <li>
+                  <a
+                    href="https://www.facebook.com/vishal.shashikant.gaikwad"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="icon-facebook2" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/mr.vishu11/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="icon-instagram" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="http://linkedin.com/in/vishal-gaikwad-83a025b1"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="icon-linkedin2" />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://github.com/vishalgofficial"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <i className="icon-github"></i>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <div className="colorlib-footer">
+              <p>
+                <small>Something coming soon !!</small>
+              </p>
+            </div>
+          </aside>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Orgnizational Experience As a Software Engginer{" "}
+                          <span>2015-present</span>
+                        </h2>
+                        <p>
+                          I have almost 5 years of experince in Product
+                          Development.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInTop"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Undergraduation at Mumbai University{" "}
+                          <span>2011-2014</span>
+                        </h2>
+                        <p>
+                          I complete my under-graduation studies with major in
+                          I.T.(Information Technology). I have taken courses
+                          like DSA, OOPs, Computer Networks, IOT, Web-Data
+                          Management over the years and have better
+                          understanding of these subjects.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry animate-box"
+                    data-animate-effect="fadeInLeft"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>
+                          Primary and Higher Education <span>2003-2011</span>
+                        </h2>
+                        <p>
+                          I have completed my Diploma in Inforamtion Technology
+                          with Distinction. During my time at school, I have
+                          developed interest in solving complex problems of the
+                          fundamental physics which helped me to improve my
+                          understanding of any problem and also my mathematical
+                          skills to actually solve the problems.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none"></div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
